b096314d8c6ec257558f64045f4a549353b974f7
David Wu <davidjwu92@gmail.com>
2020-05-04 17:45:33 -0700

    Lessons Outline Completed

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,19 +1,49 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './navbar.css'
+import M from 'materialize-css';
 
 const Navbar = () => {
+  useEffect(() => {
+    let dropdowns = document.querySelectorAll(".dropdown-trigger");
+    let options = {
+      inDuration: 300,
+      outDuration: 225,
+      belowOrigin: true
+    };
+    M.Dropdown.init(dropdowns, options);
+
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <nav className="nav-extended black">
       <div className="nav-wrapper" id="navWrapper">
         <div className="brand-logo" id="navTitle"><Link to="/">David Teaches Physics</Link></div>
-      </div>
-      <div className="nav-content">
-        <ul className="tabs tabs-transparent">
-          <li className="tab left"><Link to="/"><i className="fas fa-home"></i></Link></li>
-          <li className="tab"><Link to="/lessons">Lessons</Link></li>
-          <li className="tab"><Link to="/vocabulary">Vocabulary</Link></li>
-        </ul>
+        <div className="right">
+          <ul>
+            <li style={{height: "20px"}}><h5><a className="dropdown-trigger" href="#!"
+                data-target="lessonDropdown" onMouseEnter={e => {
+                  const inst = M.Dropdown.getInstance(e.target);
+                  inst && inst.open();
+                }}
+              >
+                LESSONS <i class="material-icons right"
+                  style={{position: "relative", padding:"0px",top: "-18px",margin: 0}}>arrow_drop_down</i>
+              </a></h5>
+              <ul id="lessonDropdown" className="dropdown-content">
+                <li><Link to="/kinematics">1. Kinematics</Link></li>
+                <li><Link to="/forces">2. Forces</Link></li>
+                <li><Link to="/impulse-momentum">3. Impulse and Momentum</Link></li>
+                <li><Link to="/work-energy">4. Work and Energy</Link></li>
+                <li><Link to="/circular-motion">5. Circular Motion</Link></li>
+                <li><Link to="/review-classical-mech">6. Classical Mechanics Review</Link></li>
+              </ul>
+            </li>
+            <li><h5><Link to="/lessons">LESSON LIST</Link></h5></li>
+            <li><h5><Link to="/vocabulary">VOCABULARY</Link></h5></li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
--- client/src/Pages/Lessons/Lessons.js
+++ client/src/Pages/Lessons/Lessons.js
@@ -5,16 +5,17 @@ import React from 'react'
 const Lessons = () => {
   return (
     <>
-      <div className="purple darken-1 white-text">
-        <h3 className="center" style={{margin:"0"}}>Classical Mechanics Lessons</h3>
+      {/* PART ONE: CLASSICAL MECHANICS */}
+      <div className="blue darken-1 white-text">
+        <h3 className="center" style={{marginTop:"0"}}>Part One: Classical Mechanics</h3>
         <h6 className="center" style={{margin:"0"}}><i>AKA Newtonian Mechanics. Part One consists of what one would expect to study in their first semester of Physics: Kinematics, Forces, Momentum/Energy, Circular Motion, and all other things pertaining to the motion of objects.</i></h6>
         <br></br>
       </div>
-      <div className="row" style={{display:"flex", flexWrap:"wrap"}}>
-          {/* KINEMATICS LESSONS */}
+      <div className="row" style={{display:"flex", flexWrap:"wrap", marginBottom:0}}>
+          {/* 1. KINEMATICS LESSONS */}
         <div className="col s12 m6 l6 blue lighten-4">
-          <h4 className="center">1. Kinematics: How do things move?</h4>
-          <h6>Lesson 1.1 Intro to Kinematics.</h6>
+          <h4 className="center">1. Kinematics</h4>
+          <h6>Lesson 1.1 Intro to Kinematics</h6>
             <div style={{textIndent: "15px"}}>
               <li>Physics as a Lab Science: Accuracy, Precision, and the SI System</li>
               <li>Vectors and Scalars</li>
@@ -41,9 +42,9 @@ const Lessons = () => {
             </div>
         </div>
           
-          {/* FORCES */}
-        <div className="col s12 m6 l6 green lighten-4">
-          <h4 className="center">2. Forces: Why do things accelerate?</h4>
+          {/* 2. FORCES */}
+        <div className="col s12 m6 l6 light-blue lighten-4">
+          <h4 className="center">2. Forces</h4>
           <h6>Lesson 2.1 Intro to Forces</h6>
             <div style={{textIndent: "15px"}}>
               <li>What are forces?</li>
@@ -74,9 +75,10 @@ const Lessons = () => {
               <li>Additional Practice</li>
             </div>
         </div>
-          {/* IMPULSE AND MOMENTUM */}
-        <div className="col s12 m6 l6 red lighten-4">
-          <h4 className="center">3. Impulse and Momentum: How do moving objects behave?</h4>
+          
+          {/* 3. IMPULSE AND MOMENTUM */}
+        <div className="col s12 m6 l6 light-blue lighten-3">
+          <h4 className="center">3. Impulse and Momentum</h4>
           <h6>Lesson 3.1 Intro to Impulse and Momentum</h6>
             <div style={{textIndent: "15px"}}>
               <li>What is Impulse?</li>
@@ -100,13 +102,14 @@ const Lessons = () => {
             </div>
         </div>
           
-          {/* WORK AND ENERGY */}
-        <div className="col s12 m6 l6 purple lighten-4">
-          <h4 className="center">4. Work and Energy: How do objects do stuff?</h4>
+          {/* 4. WORK AND ENERGY */}
+        <div className="col s12 m6 l6 blue lighten-3">
+          <h4 className="center">4. Work and Energy</h4>
           <h6>Lesson 4.1 Defining Work</h6>
             <div style={{textIndent: "15px"}}>
               <li>Work, the product of Force and Distance</li>
               <li>Work Done by Varying Forces</li>
+              <li>Power, the rate of Work-doing</li>
             </div>
           <h6>Lesson 4.2 Mechanical Energy (Kinetic and Potential)</h6>
             <div style={{textIndent: "15px"}}>
@@ -132,9 +135,9 @@ const Lessons = () => {
             </div>
         </div>
 
-          {/* CENTRIPETAL MOTION */}
-        <div className="col s12 m6 l6 amber lighten-4">
-          <h4 className="center">5. Centripetal Motion: How do objects move in circles?</h4>
+          {/* 5. CENTRIPETAL MOTION */}
+        <div className="col s12 m6 l6 blue lighten-4">
+          <h4 className="center">5. Centripetal Motion</h4>
           <h6>Lesson 5.1 Intro to Rotational Kinematics</h6>
             <div style={{textIndent: "15px"}}>
               <li>Displacement, Velocity, and Acceleration: with a twist</li>    
@@ -153,9 +156,9 @@ const Lessons = () => {
             </div>
         </div>
         
-          {/* CLASSICAL MECH REVIEW */}
-        <div className="col s12 m6 l6 teal lighten-4">
-          <h4 className="center">6. Classical Mechanics: Review and Strategies</h4>
+          {/* 6. CLASSICAL MECH REVIEW */}
+        <div className="col s12 m6 l6 light-blue lighten-4">
+          <h4 className="center">6. Classical Mechanics Review</h4>
           <h6>Lesson 6.1 Concept Map</h6>
             <div style={{textIndent: "15px"}}>
               <li>Fitting together all the pieces of the Classical Mechanics Puzzle</li>
@@ -169,14 +172,261 @@ const Lessons = () => {
         </div>
       </div>{/* END OF CLASSICAL MECHANICS DIV */}
 
-      <div className="purple darken-1 white-text">
-        <h3 className="center" style={{margin:"0"}}>Electromagnetics Lessons</h3>
+      {/* PART TWO: ELECTROMAGNETISM*/}
+      <div className="amber darken-2 white-text">
+        <h3 className="center" style={{marginTop:"0"}}>Part Two: Electromagnetism</h3>
         <h6 className="center" style={{margin:"0"}}><i>Everything from electrostatics, electrodynamics and circuits, to magnetic fields.</i></h6>
         <br></br>
       </div>
-      <div className="row" style={{display:"flex", flexWrap:"wrap"}}>
+      <div className="row" style={{display:"flex", flexWrap:"wrap", marginBottom: 0}}>
+        {/* 7. ELECTROSTATICS */}
+        <div className="col s12 m6 l6 amber lighten-4">
+          <h4 className="center">7. Electrostatics</h4>
+          <h6>Lesson 7.1 Intro to Electrostatics</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Positive and Negative Charges</li>
+              <li>Elementary Charge, <i>e</i></li>
+              <li>Conductors and Insulators</li>
+            </div>
+          <h6>Lesson 7.2 Electric Fields and Force</h6>
+            <div style={{textIndent: "15px"}}>
+                <li>Envisioning the Electric Field</li>
+                <li>Forces between charges</li>
+                <li>Coulomb's Law problems</li>
+            </div>
+          <h6>Lesson 7.3 Electric Potential</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Potential, Electric Potential Energy, and Voltage</li>
+              <li>Work in Moving Charges</li>
+            </div>
+        </div>
+        
+        {/* 8. ELECTRODYNAMICS */}
+        <div className="col s12 m6 l6 orange lighten-4">
+          <h4 className="center">8. Electrodynamics and Circuits</h4>
+          <h6>Lesson 8.1 Intro to Electrodynamics</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Electric Potential Difference and EMF</li>
+              <li>Current: the flow of charge</li>
+              <li>Resistance: the current-inhibiting factor</li>
+              <li>Ohm's Law</li>
+              <li>Revisiting Power</li>
+            </div>
+          <h6>Lesson 8.2 Introductory Circuits</h6>
+            <div style={{textIndent: "15px"}}>
+                <li>Drawing Circuit Diagrams</li>
+                <li>Resistors connected in Series vs. Parallel</li>
+                <li>The P, I, V, R Chart</li>
+            </div>
+          <h6>Lesson 8.3 Capacitors</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>What are capacitors?</li>
+              <li>Calculating Capacitance</li>
+              <li>Capacitors connected in series and parallel</li>
+            </div>
+        </div>
+      
+        {/* 9. MAGNETIC FIELDS */}
+        <div className="col s12 m6 l6 orange lighten-3">
+          <h4 className="center">9. Magnets and Magnetic Fields</h4>
+          <h6>Lesson 9.1 Magnetic Fields</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>What are magnets?</li>
+              <li>Envisioning B-Fields</li>
+            </div>
+          <h6>Lesson 9.2 B-Fields from Charges in Motion</h6>
+            <div style={{textIndent: "15px"}}>
+                <li>Cross Products and the Right Hand Rule</li>
+                <li>F<sub>B</sub> from a moving point charge</li>
+                <li>F<sub>B</sub> from current-carrying wires</li>
+                <li>Lorentz Force Law</li>
+            </div>
+          <h6>Lesson 9.3 Magnetic Flux</h6>
+            <div style={{textIndent: "15px"}}>
+                <li>Inducing Currents in Loops</li>
+                <li>Electromagnets</li>
+                <li>Building an Electric Motor</li>
+            </div>
+      </div>{/* END OF E/M */}
+        <div className="col s12 m6 l6 amber lighten-3">
 
+        </div>
       </div>
+      {/* PART THREE: HEAT AND THERMO */}
+      <div className="red darken-1 white-text">
+        <h3 className="center" style={{marginTop:"0"}}>Part Three: Heat and Thermodynamics</h3>
+        <h6 className="center" style={{margin:"0"}}><i>Everything from calorimetry to thermodynamics and heat engines.</i></h6>
+        <br></br>
+      </div>
+
+      <div className="row" style={{display:"flex", flexWrap:"wrap", marginBottom: 0}}>
+        {/* 10. CALORIMETRY */}
+        <div className="col s12 m6 l6 red lighten-4">
+          <h4 className="center">10. Calorimetry</h4>
+          <h6>Lesson 10.1 Intro to Calorimetry</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Temperature, Heat, and Internal Energy</li>
+              <li>Heat Transfer by Conduction, Convection, or Radiation</li>
+              <li>Thermal conductors vs. insulators</li>
+            </div>
+          <h6>Lesson 10.2 Specific Heat</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Specific Heat and Specific Heat Capacity</li>
+              <li>The States of Matter and Latent Heat</li>
+            </div>
+          <h6>Lesson 10.3 Calorimetry Calculations</h6>
+            <div style={{textIndent: "15px"}}>
+                <li>Mixing different substances with different temperatures</li>
+                <li>Changing States of Matter</li>
+            </div>
+        </div>
+
+        {/* 11. THERMODYNAMICS */}
+        <div className="col s12 m6 l6 pink lighten-4">
+          <h4 className="center">11. Thermodynamics</h4>
+          <h6>Lesson 11.1 The First Law of Thermodynamics</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>The Zeroeth Law of Thermodynamics</li>
+              <li>The First Law of Thermodynamics</li>
+              <li>PV Diagrams</li>
+            </div>
+          <h6>Lesson 11.2 The Second Law of Thermodynamics</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Introducing Entropy and the 2<sup>nd</sup> Law</li>
+              <li>Efficiency and Heat Engines</li>
+              <li>Perpetual Motion and the Carnot Engine</li>
+            </div>
+          <h6>Lesson 11.3 The Third Law of Thermodynamics</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Reversible vs. Irreversible Processes</li>
+              <li>Entropy as Time's "Arrow"</li>
+              <li>Heat Death, or the Big Freeze</li>
+            </div>
+        </div>
+      </div>{/* END OF HEAT AND THERMO */}
+
+      {/* PART FOUR: WAVES AND OPTICS */}
+      <div className="green darken-1 white-text">
+        <h3 className="center" style={{marginTop:"0"}}>Part Four: Periodic Motion, Waves, and Optics</h3>
+        <h6 className="center" style={{margin:"0"}}><i>From pendulums to Sound Waves and Electromagnetic Radiation, waves and optics are an incredibly important part of our daily lives.</i></h6>
+        <br></br>
+      </div>
+      <div className="row" style={{display:"flex", flexWrap:"wrap", marginBottom: 0}}>
+        {/* 12. PERIODIC MOTION */}
+        <div className="col s12 m6 l6 green lighten-4">
+          <h4 className="center">12. Periodic Motion</h4>
+          <h6>Lesson 12.1 Simple Harmonic Motion</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Revisiting Hooke's Law: Restorative Forces</li>
+              <li>Frequency and Period</li>
+              <li>Sine and Cosine Graphs</li>
+            </div>
+          <h6>Lesson 12.2 Analyzing Simple Harmonic Oscillators</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Simple Harmonic Oscillators</li>
+              <li>Bob on a spring problems</li>
+              <li>Pendulum problems</li>
+              <li>Satellites and Orbits Revisited</li>
+              <li>Damped Harmonic Oscillators</li>
+            </div>
+        </div> 
+
+        {/* 13. MECHANICAL WAVES INTRO */}
+        <div className="col s12 m6 l6 light-green lighten-4">
+          <h4 className="center">13. Intro to Waves</h4>
+          <h6>Lesson 13.1 Properties of Wave Pulses</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Transverse, Longitudinal, and Surface Waves</li>
+              <li>Calculating Wave Speed using displacement and time</li>
+              <li>Boundaries and varying media</li>
+              <li>Reflection, Inteference, and Superposition</li>
+            </div>
+          <h6>Lesson 13.2 Periodic Waves</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Frequency, Period, and Wavelength</li>
+              <li>Amplitude and Wave Energy</li>
+              <li>Graphing waves</li>
+            </div>
+        </div> 
+
+        {/* 14. PROPERTIES OF SOUND */}
+        <div className="col s12 m6 l6 green lighten-3">
+          <h4 className="center">14. The Physics of Sound</h4>
+          <h6>Lesson 14.1 Characteristics of Musical Sounds</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Frequency and pitch</li>
+              <li>Amplitude and the Decibel Scale</li>
+            </div>
+          <h6>Lesson 14.2 Musical Instruments and Resonance</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Resonance, Harmonics, and Human Hearing</li>
+              <li>Standing Waves: harmonics on a string</li>
+              <li>Standing Waves: harmonics open-pipe and closed-pipe</li>
+            </div>
+          <h6>Lesson 14.3 Properties of Sounds</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Sound speeds in different media</li>
+              <li>Soundproofing: Transmission versus Reflection</li>
+              <li>The Doppler Effect</li>
+              <li>The sound barrier and sonic booms</li>
+            </div>
+        </div> 
+
+        {/* 15. EM RADIATION */}
+        <div className="col s12 m6 l6 light-green lighten-3">
+          {/* LIGHT AND EMR */}
+          <h4 className="center">15. Light and Electromagnetic Radiation</h4>
+          <h6>Lesson 15.1 The Electromagnetic Spectrum</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>The speed of light in a vacuum</li>
+              <li>Frequency and wavelength of EMR</li>
+              <li>Bands of the Electromagnetic Spectrum</li>
+            </div>
+          <h6>Lesson 15.2 Properties of Light: Reflection and Refraction</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>The Law of Reflection: Diffuse versus Smooth Reflection</li>
+              <li>Light speeds in different media</li>
+              <li>Refraction and Snell's Law</li>
+              <li>Total Internal Reflection</li>
+            </div>
+          <h6>Lesson 15.3 Properties of Light: Polarization, Diffraction, and Interference</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Polarization of light and its applications</li>
+              <li>Interference and Iridescence</li>
+              <li>Diffraction Gratings</li>
+            </div>
+          {/* <h6>Lesson 15.5 Wave-Particle Duality</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Young's Double-Slit Experiment</li>
+              <li>The Photoelectric Effect</li>
+              <li>The de Broglie Hypothesis and Matter Waves</li>
+            </div> */}
+        </div>
+
+        {/* 16. OPTICS AND LENSES */}
+        <div className="col s12 m6 l6 green lighten-4">
+          <h4 className="center">16. Mirrors, Lenses, and Optics</h4>
+          <h6>Lesson 16.1 Mirrors</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>Foci and Ray Diagrams</li>
+              <li>Concave Mirrors and Magnification</li>
+              <li>Convex Mirrors</li>
+            </div>
+          <h6>Lesson 16.2 Lenses</h6>
+            <div style={{textIndent: "15px"}}>
+              <li>More Ray Diagrams</li>
+              <li>Convex Lenses</li>
+              <li>Concave Lenses</li>
+              <li>Telescopes and Microscopes</li>
+            </div>
+        </div>
+
+        
+        <div className="col s12 m6 l6 light-green lighten-4">
+        </div>
+
+      </div> {/* END OF WAVES AND OPTICS */}
+
     </>
     )
 }
