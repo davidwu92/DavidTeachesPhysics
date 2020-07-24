94b4e704b62b239bc0fe05e8506c7a087ee6a09f
David Wu <davidjwu92@gmail.com>
2020-07-24 10:24:54 -0700

    displacement velocity acc

--- client/src/Pages/Lessons/ClassicalMechanics/Kinematics/Lesson1_1.js
+++ client/src/Pages/Lessons/ClassicalMechanics/Kinematics/Lesson1_1.js
@@ -99,6 +99,7 @@ const Lesson1_1 = () => {
             </tbody>
           </table>
           <p><em>Buttercup has the most interesting set of data; while her accuracy is relatively high, note that data from each of her trials were highly dispersed.</em></p>
+          <h1 className="grey-text center grey lighten-2">diagrams and practice</h1>
         </div> {/* END OF ACCURACY/PRECISION */}
 
         <div id="section2" className="blue lighten-5" style={{padding:"1vh 6vw"}}>
@@ -112,26 +113,93 @@ const Lesson1_1 = () => {
               </div>
             </div>
             <div className="col s12 m6 l6">
-            <h6  className="center">A <b>vector</b> is any mathematical quantity that has magnitude and direction.</h6>
-              <div style={{padding:"0 5vw"}}>
-              <p style={{textIndent: "30px"}}>Vectors care about a number, unit, and direction. The most common examples of vectors are velocity (<em>v</em>) and acceleration (<em>a</em>), both of which we'll be discussing at length in the next section.</p>
-              <p style={{textIndent: "30px"}}>Note that <em>changes in scalars</em> are mathematically considered vectors! If <em>d</em> represents the amount of money in your wallet, any amount of spent or incoming cash is, respectively, a negative or positive change in <em>d</em>. If the temperature of your oven, <em>T</em>, changes from 300<sup>o</sup>F to 340<sup>o</sup>F, that change in <em>T</em> is indeed a vector: positive 40 degrees.</p>
-              </div>
+              <h6  className="center">A <b>vector</b> is any mathematical quantity that has magnitude and direction.</h6>
+                <div style={{padding:"0 5vw"}}>
+                  <p style={{textIndent: "30px"}}>Vectors care about a number, unit, and direction. The most common examples of vectors are velocity (<em>v</em>) and acceleration (<em>a</em>), both of which we'll be discussing at length in the next section.</p>
+                  <p style={{textIndent: "30px"}}>Note that <em>changes in scalars</em> are mathematically considered vectors! If <em>d</em> represents the amount of money in your wallet, any amount of spent or incoming cash is, respectively, a negative or positive change in <em>d</em>. If the temperature of your oven, <em>T</em>, changes from 300<sup>o</sup>F to 340<sup>o</sup>F, that change in <em>T</em> is indeed a vector: positive 40 degrees.</p>
+                </div>
             </div>
           </div>
 
-          <h5>Analyzing Vectors</h5>
+          <h5>Analyzing 2D Vectors</h5>
           <p style={{textIndent: "30px"}}>Mathematical operations with scalars generally aren't any more complicated than basic arithmetic. Add 50 grams of sugar to 500 grams of water, and you get an unsurprising 550 grams' worth of sugary solution. A 4-foot x 5-foot rectangular space has an area of 20 ft<sup>2</sup>. When adding, subtracting, or multiplying vectors, however... we'll need to pull in bigger math guns, and use trigonometry to our advantage.</p>
           <p>We generally represent 2-dimensional vectors in one of two ways: either with a magnitude and angle above the horizontal (the positive x-axis), or with two component vectors.</p>
-          <h5><InlineMath>{String.raw`\vec{v}=v_x + v_y`}</InlineMath></h5>
+          <h5><InlineMath>{String.raw`\vec{v}=\vec{v_x} + \vec{v_y} = v_x\hat{i} + v_y\hat{j}`}</InlineMath></h5>
+          <h6><InlineMath>{String.raw`v_x=|v|\cos{\theta}`}</InlineMath></h6>
+          <h6><InlineMath>{String.raw`v_y=|v|\sin{\theta}`}</InlineMath></h6>
+          <h1 className="grey-text center grey lighten-2">diagrams and practice</h1>
         </div>{/* END OF VECTORS/SCALARS */}
 
+        {/* displacement, v, a */}
         <div id="section3" className="green lighten-5" style={{padding:"1vh 6vw"}}>
           <h4 className="green-text text-darken-3">Displacement, Velocity, and Acceleration</h4>
+          <p style={{textIndent: "30px"}}><b>These fundamental kinematics quantities are absolutely essential to your success as a physics student.</b> I'm not exaggerating when I say that almost all of the new ideas you'll study in physics will rely on one or more of these quantities! Mastering these equations, definitions, and problem-solving techniques early on will save you many days' worth of headaches.</p>
+
+          <div className="row">
+            <div className="col s12 m7 l7">
+              <p><b>Displacement</b> is the vector equivalent of <b>distance</b>; both are measured in meters (m). While distance, <em>d</em>, represents how far meters an object has travelled (or how far apart two points are), displacement is defined as the <em>change in position</em>. If "x" represents where an object lies along a horizontal axis, we use Δx ("delta-x") to represent the <em>change in "x"</em> AKA the horizontal displacement.</p>
+              <div className="row">
+                <div className="col s6 m6 l6">
+                  <h6 className="center"><InlineMath>{String.raw`Δx=x_f - x_i`}</InlineMath></h6>
+                </div>
+                <div className="col s6 m6 l6">
+                  <h6 className="center"><InlineMath>{String.raw`Δy=y_f - y_i`}</InlineMath></h6>
+                </div>
+              </div>
+              
+            </div>
+            <div className="col s12 m5 l5">
+              <h1 className="grey-text">Displacement sim</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s12 m7 l7">
+              <p><b>Velocity</b> is the vector equivalent of <b>speed</b>; both are measured in meters per second (<InlineMath>{String.raw`\frac{m}{s}`}</InlineMath>). Since time, <em>t</em>, is measured in seconds...</p>
+              <div className="row">
+                <div className="col s6 m6 l6">
+                  <h6 className="center"><InlineMath>{String.raw`\vec{v_x}=\frac{Δx}{t}`}</InlineMath></h6>
+                  <h6 className="center"><InlineMath>{String.raw`\vec{v_y}=\frac{Δy}{t}`}</InlineMath></h6>
+                  <em>Since velocity is a vector, we can break down its x- and y- components</em>
+                </div>
+                <div className="col s6 m6 l6">
+                  <h6 className="center"><InlineMath>{String.raw`s = \frac{d}{t}`}</InlineMath></h6>
+                  <em>Speed "s" is a scalar, as is distance "d"; neither of them can be negative values.</em>
+                </div>
+              </div>
+              
+            </div>
+            <div className="col s12 m5 l5">
+              <h1 className="grey-text">Velocity sim</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s12 m7 l7">
+              <p><b>Acceleration</b> is defined as the rate of change in velocity; therefore, it is measured in meters per second-squared (<InlineMath>{String.raw`\frac{m}{s^2}`}</InlineMath>).</p>
+              <div className="row">
+                <div className="col s6 m6 l6">
+                  <h6 className="center"><InlineMath>{String.raw`a = \frac{Δ\vec{v}}{t}=\frac{\vec{v_f} - \vec{v_i}}{t}`}</InlineMath></h6>
+                </div>
+                <div className="col s6 m6 l6">
+                  <h6 className="center"><InlineMath>{String.raw`Δ\vec{v_x}=\vec{v_{xf}} - \vec{v_{xi}}`}</InlineMath></h6>
+                  <h6 className="center"><InlineMath>{String.raw`Δ\vec{v_y}=\vec{v_{yf}} - \vec{v_{yi}}`}</InlineMath></h6>
+                </div>
+              </div>
+              
+            </div>
+            <div className="col s12 m5 l5">
+              <h1 className="grey-text">Acceleration sim</h1>
+            </div>
+          </div>
+
+
+          <h1 className="grey-text center grey lighten-2">diagrams and practice</h1>
         </div>{/* END OF x, v, a */}
 
         <div id="section4" className="blue lighten-5" style={{padding:"1vh 6vw"}}>
           <h4 className="blue-text text-darken-3">Freefall and Acceleration due to Gravity</h4>
+
+
+          <h1 className="grey-text center grey lighten-2">diagrams and practice</h1>
         </div>{/* END OF ACCURACY/PRECISION */}
 
         
